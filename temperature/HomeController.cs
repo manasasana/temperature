5a23100f5dbeaea35807c5b26da2b612cfe92c63
manasasana <manasaadupa123@gmail.com>
2022-09-01 09:46:32 +0530

    Add project files.

---
+++ temperature/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace temperature
+{
+    public class HomeController : Controller
+    {
+        public string check(int temp)
+        {
+            if (temp > 96 && temp <= 99)
+                return $"normal";
+            if (temp > 99 && temp <= 101)
+                return $"Take home care";
+            if (temp > 101)
+                return $"Medical attention needed";
+            else
+                return $"check temperature again";
+        }
+    }
+}
